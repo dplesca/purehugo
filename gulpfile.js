98bb6e3f30cc44c12848acf90b912f3e6fa69619
Dragos Plesca <dragos.plesca@gmail.com>
2015-03-21 00:45:30 +0200

    Adding gulp for combine and minifying of assets

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp')
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	minifyCSS = require('gulp-minify-css');
+
+gulp.task('compress', function() {
+  gulp.src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/rainbow-custom.min.js', 'assets/js/scripts.js'])
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('static/js/'));
+  gulp.src(['assets/css/blog.css', 'assets/css/syntax-highlighter.css'])
+    .pipe(concat('all.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('static/css/'));
+});
